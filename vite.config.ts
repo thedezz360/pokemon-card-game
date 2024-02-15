56b6e591c4f8d58334fa64c349f17ed36e70a703
Daniel <thedezz360@gmail.com>
2024-02-15 14:03:55 +0100

    change viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	base:"/pokemon-card-game/",
-	plugins: [react()],
+export default defineConfig(({command})=>{
+	if(command === "build"){
+		return {
+			base:"/pokemon-card-game/",
+			plugins: [react()],
+		};
+	}else{
+		return {
+			plugins: [react()],
+		};
+	}
 });
