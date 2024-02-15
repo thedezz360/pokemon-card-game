9f8b7d41284f2ed99501e1c238b490d049226304
Daniel <thedezz360@gmail.com>
2024-02-15 13:16:34 +0100

    fix viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base:"pokemon-card-game",
+	base:"/pokemon-card-game/",
 	plugins: [react()],
 });
